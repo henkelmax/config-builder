c26651989a291478e27653eacdf8f6c2fe75b9dc
Max Henkel <max@maxhenkel.de>
2023-08-05 15:24:18 +0200

    Improve entry order test

--- src/test/java/de/maxhenkel/configbuilder/OrderTest.java
+++ src/test/java/de/maxhenkel/configbuilder/OrderTest.java
@@ -17,8 +17,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class OrderTest {
 
     @Test
-    @DisplayName("Save and read")
-    void saveAndRead(@TempDir Path tempDir) throws IOException {
+    @DisplayName("Save and read without removing unused entries")
+    void saveAndReadWithoutUnused(@TempDir Path tempDir) throws IOException {
         Path config = TestUtils.randomConfigName(tempDir);
 
         StringBuilder configString = new StringBuilder();
@@ -37,18 +37,14 @@ public class OrderTest {
         commentedProperties.load(in1);
         commentedProperties.save(Files.newOutputStream(config.toFile().toPath()));
 
-        ConfigBuilder.build(config, false, builder -> {
+        ConfigBuilder.build(config, builder -> {
             ConfigEntry<Integer> one = builder.integerEntry("test_one", 1, 0, 10);
             ConfigEntry<String> two = builder.stringEntry("test_two", "two");
             ConfigEntry<Boolean> three = builder.booleanEntry("test_three", true);
             return null;
         });
 
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        TestUtils.sleep();
 
         ConfigBuilderImpl builder = TestUtils.createBuilder(config);
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -64,6 +60,50 @@ public class OrderTest {
         expectedOutput.append("unused1=nothing");
         expectedOutput.append("\n");
         expectedOutput.append("unused2=nothing");
+
+        assertEquals(expectedOutput.toString().replace("\r\n", "\n").trim(), baos.toString().replace("\r\n", "\n").trim());
+    }
+
+    @Test
+    @DisplayName("Save and read with removing unused entries")
+    void saveAndReadWithUnused(@TempDir Path tempDir) throws IOException {
+        Path config = TestUtils.randomConfigName(tempDir);
+
+        StringBuilder configString = new StringBuilder();
+        configString.append("test_three=true");
+        configString.append("\n");
+        configString.append("test_two=two");
+        configString.append("\n");
+        configString.append("test_one=1");
+        configString.append("\n");
+        configString.append("unused1=nothing");
+        configString.append("\n");
+        configString.append("unused2=nothing");
+
+        ByteArrayInputStream in1 = new ByteArrayInputStream(configString.toString().getBytes(StandardCharsets.UTF_8));
+        CommentedProperties commentedProperties = new CommentedProperties();
+        commentedProperties.load(in1);
+        commentedProperties.save(Files.newOutputStream(config.toFile().toPath()));
+
+        ConfigBuilder.build(config, true, builder -> {
+            ConfigEntry<Integer> one = builder.integerEntry("test_one", 1, 0, 10);
+            ConfigEntry<String> two = builder.stringEntry("test_two", "two");
+            ConfigEntry<Boolean> three = builder.booleanEntry("test_three", true);
+            return null;
+        });
+
+        TestUtils.sleep();
+
+        ConfigBuilderImpl builder = TestUtils.createBuilder(config);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        builder.config.properties.save(baos);
+
+        StringBuilder expectedOutput = new StringBuilder();
+        expectedOutput.append("test_one=1");
+        expectedOutput.append("\n");
+        expectedOutput.append("test_two=two");
+        expectedOutput.append("\n");
+        expectedOutput.append("test_three=true");
 
         assertEquals(expectedOutput.toString().replace("\r\n", "\n").trim(), baos.toString().replace("\r\n", "\n").trim());
     }
