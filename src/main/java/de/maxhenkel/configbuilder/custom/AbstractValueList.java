3640bad31ac2ba333e6fb7b0054f9c31bf565ed9
Max Henkel <max@maxhenkel.de>
2023-08-07 16:33:54 +0200

    Create abstract value list

---
+++ src/main/java/de/maxhenkel/configbuilder/custom/AbstractValueList.java
@@ -0,0 +1,139 @@
+package de.maxhenkel.configbuilder.custom;
+
+import java.util.*;
+
+/**
+ * An unmodifiable list intended to be used as a config entry.
+ */
+public abstract class AbstractValueList<T> implements List<T> {
+
+    protected final List<T> list;
+
+    protected AbstractValueList(T... values) {
+        this(Arrays.asList(values));
+    }
+
+    protected AbstractValueList(List<T> values) {
+        list = Collections.unmodifiableList(values);
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return list.contains(o);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return list.iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return list.toArray();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return list.toArray(a);
+    }
+
+    @Override
+    public boolean add(T s) {
+        return throwException();
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return throwException();
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return list.containsAll(c);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return throwException();
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return throwException();
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return throwException();
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return throwException();
+    }
+
+    @Override
+    public void clear() {
+        throwException();
+    }
+
+    @Override
+    public T get(int index) {
+        return list.get(index);
+    }
+
+    @Override
+    public T set(int index, T element) {
+        return throwException();
+    }
+
+    @Override
+    public void add(int index, T element) {
+        throwException();
+    }
+
+    @Override
+    public T remove(int index) {
+        return throwException();
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return list.indexOf(o);
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return list.lastIndexOf(o);
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return list.listIterator();
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return list.listIterator(index);
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return list.subList(fromIndex, toIndex);
+    }
+
+    private static <T> T throwException() {
+        throw new UnsupportedOperationException("Can't modify config entries");
+    }
+
+}
--- src/main/java/de/maxhenkel/configbuilder/custom/StringList.java
+++ src/main/java/de/maxhenkel/configbuilder/custom/StringList.java
@@ -7,16 +7,14 @@ import java.util.*;
  * <br/>
  * The list is serialized as a string with the elements separated by a semicolon.
  */
-public class StringList implements List<String> {
-
-    protected final List<String> list;
+public class StringList extends AbstractValueList<String> {
 
     protected StringList(String... values) {
-        this(Arrays.asList(values));
+        super(values);
     }
 
     protected StringList(List<String> values) {
-        list = Collections.unmodifiableList(values);
+        super(values);
     }
 
     public static StringList of(String... values) {
@@ -25,125 +23,6 @@ public class StringList implements List<String> {
 
     public static StringList of(List<String> values) {
         return new StringList(values);
-    }
-
-    @Override
-    public int size() {
-        return list.size();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return list.isEmpty();
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        return list.contains(o);
-    }
-
-    @Override
-    public Iterator<String> iterator() {
-        return list.iterator();
-    }
-
-    @Override
-    public Object[] toArray() {
-        return list.toArray();
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        return list.toArray(a);
-    }
-
-    @Override
-    public boolean add(String s) {
-        return throwException();
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return throwException();
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return list.containsAll(c);
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends String> c) {
-        return throwException();
-    }
-
-    @Override
-    public boolean addAll(int index, Collection<? extends String> c) {
-        return throwException();
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        return throwException();
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        return throwException();
-    }
-
-    @Override
-    public void clear() {
-        throwException();
-    }
-
-    @Override
-    public String get(int index) {
-        return list.get(index);
-    }
-
-    @Override
-    public String set(int index, String element) {
-        return throwException();
-    }
-
-    @Override
-    public void add(int index, String element) {
-        throwException();
-    }
-
-    @Override
-    public String remove(int index) {
-        return throwException();
-    }
-
-    @Override
-    public int indexOf(Object o) {
-        return list.indexOf(o);
-    }
-
-    @Override
-    public int lastIndexOf(Object o) {
-        return list.lastIndexOf(o);
-    }
-
-    @Override
-    public ListIterator<String> listIterator() {
-        return list.listIterator();
-    }
-
-    @Override
-    public ListIterator<String> listIterator(int index) {
-        return list.listIterator(index);
-    }
-
-    @Override
-    public List<String> subList(int fromIndex, int toIndex) {
-        return list.subList(fromIndex, toIndex);
-    }
-
-    private static <T> T throwException() {
-        throw new UnsupportedOperationException("Can't modify config entries");
     }
 
 }
